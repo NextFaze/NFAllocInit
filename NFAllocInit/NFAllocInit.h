6c0e4b886483a75ef235f1afd91abece0652dcc1
Ric Santos <rics@ntos.me>
2012-12-05 09:18:59 +1030

    Aded header file NFAllocInit that imports all headers.

---
+++ NFAllocInit/NFAllocInit.h
@@ -0,0 +1,14 @@
+//
+//  NFAllocInit.h
+//  NFAllocInit
+//
+//  Created by Ricardo Santos on 5/12/12.
+//  Copyright (c) 2012 NextFaze SD. All rights reserved.
+//
+
+#ifndef NFAllocInit_NFAllocInit_h
+#define NFAllocInit_NFAllocInit_h
+
+
+
+#endif
