098feb0a4bc2b323dd8259dd6ef96cf52a64cd83
Ric Santos <rics@ntos.me>
2012-12-06 10:04:23 +1030

    Updated README

--- NFAllocInit/NFAllocInit.h
+++ NFAllocInit/NFAllocInit.h
@@ -1,14 +1,17 @@
 //
 //  NFAllocInit.h
-//  NFAllocInit
 //
-//  Created by Ricardo Santos on 5/12/12.
 //  Copyright (c) 2012 NextFaze SD. All rights reserved.
 //
 
-#ifndef NFAllocInit_NFAllocInit_h
-#define NFAllocInit_NFAllocInit_h
-
-
-
-#endif
+#import "NFDebugging.h"
+#import "NFResourceUtils.h"
+#import "NFViewUtils.h"
+#import "NSArray+FTW.h"
+#import "NSData+Base64.h"
+#import "NSString+Additions.h"
+#import "UIImage+Alpha.h"
+#import "UIImage+Resize.h"
+#import "UIImage+RoundedCorner.h"
+#import "UIView+Additions.h"
+#import "UIView+FrameAdjustments.h"
--- README.md
+++ README.md
@@ -2,3 +2,16 @@ NFAllocInit
 ===========
 
 The starting point for an iOS app - helper classes and the like
+
+Integration instructions
+========================
+
+Add submodule to your project.
+
+$ git submodule add git@github.com:NextfazeSD/NFAllocInit.git Frameworks/NFAllocInit
+
+Drag the NFAllocInit.xcodeproj project file from Finder to the Frameworks folder in your project tree.
+
+Add to target's Target Dependencies in Build Phases, and also Link Binary with Libraries.
+
+Add to target linker flags '-ObjC'
