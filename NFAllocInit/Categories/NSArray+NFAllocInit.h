70513533bc163abb588f71f9030fff2c3030edcd
Andrew Williams <awilliams@nextfaze.com>
2013-09-17 15:50:19 +0930

    added a bunch of methods inspired by ruby Enumerable to NSArray category

--- NFAllocInit/Categories/NSArray+NFAllocInit.h
+++ NFAllocInit/Categories/NSArray+NFAllocInit.h
@@ -7,8 +7,38 @@
 
 #import <Foundation/Foundation.h>
 
+// informal NSComparisonMethods
+@protocol NFAllocInitComparisonMethods <NSObject>
+- (BOOL)isEqualTo:(id)object;
+- (BOOL)isGreaterThan:(id)object;
+- (BOOL)isLessThan:(id)object;
+@end
+
+typedef BOOL (^NFAllocInitTestBlock)(id object);
+typedef id (^NFAllocInitObjectBlock)(id object);
+typedef id<NFAllocInitComparisonMethods> (^NFAllocInitComparableObjectBlock)(id object);
+
 @interface NSArray (NFAllocInit)
 - (id)firstObject;
 - (id)objectAtIndexFTW:(NSUInteger)index;
 - (NSArray *)shuffle;
+
+// inspired by ruby Enumerable
+- (BOOL)all:(NFAllocInitTestBlock)block;
+- (BOOL)any:(NFAllocInitTestBlock)block;
+- (NSArray *)collect:(NFAllocInitObjectBlock)block;
+- (NSArray *)map:(NFAllocInitObjectBlock)block;
+- (void)cycle:(int)count block:(NFAllocInitObjectBlock)block;
+- (id)detect:(NFAllocInitTestBlock)block;
+- (id)find:(NFAllocInitTestBlock)block;
+- (NSArray *)findAll:(NFAllocInitTestBlock)block;
+- (NSArray *)select:(NFAllocInitTestBlock)block;
+- (NSArray *)first:(int)count;
+- (NSDictionary *)groupBy:(NFAllocInitObjectBlock)block;
+- (BOOL)one:(NFAllocInitTestBlock)block;
+- (NSArray *)partition:(NFAllocInitTestBlock)block;
+- (NSArray *)sortBy:(NFAllocInitComparableObjectBlock)block;
+
 @end
+
+
--- NFAllocInit/Categories/NSArray+NFAllocInit.m
+++ NFAllocInit/Categories/NSArray+NFAllocInit.m
@@ -30,4 +30,154 @@
     return new;
 }
 
+- (BOOL)all:(NFAllocInitTestBlock)block {
+    for(id object in self) {
+        BOOL result = block(object);
+        if(!result) return NO;
+    }
+    return YES;
+}
+
+- (BOOL)any:(NFAllocInitTestBlock)block {
+    for(id object in self) {
+        BOOL result = block(object);
+        if(result) return YES;
+    }
+    return NO;
+}
+
+- (NSArray *)collect:(NFAllocInitObjectBlock)block {
+    NSMutableArray *array = [NSMutableArray arrayWithCapacity:self.count];
+    for(id object in self) {
+        id result = block(object);
+        if(result == nil)
+            result = [NSNull null];
+        [array addObject:result];
+    }
+    return array;
+}
+
+- (NSArray *)map:(NFAllocInitObjectBlock)block {
+    return [self collect:block];
+}
+
+- (void)cycle:(int)count block:(NFAllocInitObjectBlock)block {
+    while(count-- > 0) {
+        for(id object in self)
+            block(object);
+    }
+}
+
+- (id)detect:(NFAllocInitTestBlock)block {
+    for(id object in self) {
+        if(block(object))
+            return object;
+    }
+    return nil;
+}
+
+- (id)find:(NFAllocInitTestBlock)block {
+    return [self detect:block];
+}
+
+- (NSArray *)findAll:(NFAllocInitTestBlock)block {
+    NSMutableArray *array = [NSMutableArray array];
+    for(id object in self) {
+        if(block(object))
+            [array addObject:object];
+    }
+    return array;
+}
+
+- (NSArray *)select:(NFAllocInitTestBlock)block {
+    return [self findAll:block];
+}
+
+- (NSArray *)first:(int)count {
+    NSMutableArray *array = [NSMutableArray array];
+    for(int i = 0; i < count && i < self.count; i++) {
+        [array addObject:[self objectAtIndex:i]];
+    }
+    return array;
+}
+
+// Groups the collection by result of the block. Returns a hash where the keys are the
+// evaluated result from the block and the values are arrays of elements in the collection that correspond to the key.
+- (NSDictionary *)groupBy:(NFAllocInitObjectBlock)block {
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    for(id object in self) {
+        id key = block(object);
+        if(key == nil) key = [NSNull null];
+        
+        NSMutableArray *array = [dict objectForKey:key];
+        if(array == nil) {
+            array = [NSMutableArray arrayWithObject:object];
+            [dict setObject:array forKey:key];
+        }
+        else {
+            [array addObject:object];
+        }
+    }
+    return dict;
+}
+
+// Passes each element of the collection to the given block. The method returns true if the block returns true exactly once.
+- (BOOL)one:(NFAllocInitTestBlock)block {
+    int count = 0;
+    for(id object in self) {
+        if(block(object)) {
+            if(++count > 1)
+                return NO;
+        }
+    }
+    return count == 1;
+}
+
+// Returns two arrays, the first containing the elements of enum for which the block evaluates to true, the second containing the rest.
+- (NSArray *)partition:(NFAllocInitTestBlock)block {
+    NSMutableArray *arrayTrue = [NSMutableArray array];
+    NSMutableArray *arrayFalse = [NSMutableArray array];
+    NSMutableArray *partition = [NSMutableArray arrayWithObjects:arrayTrue, arrayFalse, nil];
+
+    for(id object in self) {
+        if(block(object)) {
+            [arrayTrue addObject:object];
+        } else {
+            [arrayFalse addObject:object];
+        }
+    }
+    return partition;
+}
+
+- (NSArray *)sortBy:(NFAllocInitComparableObjectBlock)block {
+    NSMutableArray *tuples = [NSMutableArray arrayWithCapacity:self.count];
+    for(id object in self) {
+        id value = block(object);
+        if(value == nil)
+            value = [NSNull null];
+        
+        [tuples addObject:@[object, value]];
+    }
+    
+    [tuples sortUsingComparator:^NSComparisonResult(NSArray *obj1, NSArray *obj2) {
+        id<NFAllocInitComparisonMethods> value1 = [obj1 objectAtIndex:1];
+        id<NFAllocInitComparisonMethods> value2 = [obj2 objectAtIndex:1];
+        
+        if([value1 isKindOfClass:[NSNull class]])
+            value1 = nil;
+        if([value2 isKindOfClass:[NSNull class]])
+            value2 = nil;
+        
+        return (value1 == value2 ? NSOrderedSame :
+                value1 == nil && value2 != nil ? NSOrderedAscending :
+                value1 != nil && value2 == nil ? NSOrderedDescending :
+                [value1 isLessThan:value2] ? NSOrderedAscending :
+                [value1 isGreaterThan:value2] ? NSOrderedDescending : NSOrderedSame);
+    }];
+    
+    return [tuples collect:^id(NSArray *object) {
+        return [object objectAtIndex:0];
+    }];
+}
+
 @end
