ceae6f14b74839dfec95561122edfc75eeaa76bd
Ric Santos <rics@ntos.me>
2013-10-28 09:23:08 +1030

    Fix for NFAudioPlayer, change to AudioSession type (was not respecting mute switch).

--- NFAllocInit/NFAudioPlayer.m
+++ NFAllocInit/NFAudioPlayer.m
@@ -33,7 +33,7 @@
     
     self.players = [NSMutableArray array];
     
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategorySoloAmbient error:nil];
     [[AVAudioSession sharedInstance] setActive:YES error: nil];
     UInt32 route = kAudioSessionOverrideAudioRoute_Speaker;
     AudioSessionSetProperty (kAudioSessionProperty_OverrideAudioRoute, sizeof(route), &route);
