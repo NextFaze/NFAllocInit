00fcbf294627d3cd81aba5e3d53249aa962e4360
Ricardo Santos <rsantos@nextfaze.com>
2014-02-03 16:50:18 +1030

    added blurredImage to UIImage+NFAllocInit category

--- NFAllocInit/Categories/UIImage+NFAllocInit.h
+++ NFAllocInit/Categories/UIImage+NFAllocInit.h
@@ -12,4 +12,6 @@
 
 @property (nonatomic, readonly) CGFloat aspect;
 
+- (UIImage *)blurredImage;
+
 @end
--- NFAllocInit/Categories/UIImage+NFAllocInit.m
+++ NFAllocInit/Categories/UIImage+NFAllocInit.m
@@ -14,4 +14,33 @@
     return self.size.width / self.size.height;
 }
 
+- (UIImage *)blurredImage
+{
+    // create our blurred image
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CIImage *inputImage = [CIImage imageWithCGImage:self.CGImage];
+    
+    CIFilter *affineClampFilter = [CIFilter filterWithName:@"CIAffineClamp"];
+    
+    CGAffineTransform xform = CGAffineTransformMakeScale(1.0, 1.0);
+    [affineClampFilter setValue:[NSValue valueWithBytes:&xform objCType:@encode(CGAffineTransform)] forKey:@"inputTransform"];
+    [affineClampFilter setValue:inputImage forKey:kCIInputImageKey];
+    CIImage *clampedImage = [affineClampFilter valueForKey:kCIOutputImageKey];
+    
+    // setting up Gaussian Blur (we could use one of many filters offered by Core Image)
+    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [filter setValue:clampedImage forKey:kCIInputImageKey];
+    [filter setValue:[NSNumber numberWithFloat:15.0f] forKey:@"inputRadius"];
+    CIImage *result = [filter valueForKey:kCIOutputImageKey];
+    
+    // CIGaussianBlur has a tendency to shrink the image a little,
+    // this ensures it matches up exactly to the bounds of our original image
+    CGImageRef cgImage = [context createCGImage:result fromRect:[inputImage extent]];
+    
+    UIImage *image = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);
+    
+    return image;
+}
+
 @end
