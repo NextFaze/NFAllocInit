7c00243dbc43127243067e63a0bb8b004aa144e5
Ricardo Santos <rsantos@nextfaze.com>
2015-04-28 11:26:26 +0930

    added category UIImage+Orientation

---
+++ NFAllocInit/Categories/UIImage+Orientation.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Orientation.h
+//  iQNECT
+//
+//  Created by Ricardo Santos on 15/05/2014.
+//  Copyright (c) 2014 NextFaze Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Orientation)
+
+- (UIImage *)scaleAndRotateToMaxSize:(int)sizeInPixels;
+
+@end
