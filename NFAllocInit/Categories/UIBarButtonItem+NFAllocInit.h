cc2667aaba1215e91b2b028b072aa3eca1f55407
Shane Woolcock <swoolcock@nextfaze.com>
2017-05-18 12:56:54 +0930

    Add convenience init to UIBarButtonItem for creating fixed space items with a specified width

---
+++ NFAllocInit/Categories/UIBarButtonItem+NFAllocInit.h
@@ -0,0 +1,19 @@
+//
+//  UIBarButtonItem+NFAllocInit.h
+//  NFAllocInit
+//
+//  Created by swoolcock on 18/5/17.
+//  Copyright © 2017 NextFaze SD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIBarButtonItem (NFAllocInit)
+
+- (instancetype)initWithFixedSpaceWidth:(CGFloat)fixedSpaceWidth;
+
+@end
+
+NS_ASSUME_NONNULL_END
