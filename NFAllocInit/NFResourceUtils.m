5af7c4d88b5b4ae202a3a7e0d2df9b6cf5325af8
Dan Silk <SilkyPantsDan@users.noreply.github.com>
2019-11-14 10:11:22 +1030

    Remove TLD max limit
    As we now have longer than 4 TLDs (current max is 24)

--- NFAllocInit/NFResourceUtils.m
+++ NFAllocInit/NFResourceUtils.m
@@ -120,7 +120,7 @@
 {
     // source: http://stackoverflow.com/a/3638271 
     BOOL stricterFilter = YES; // Discussion http://blog.logichigh.com/2010/09/02/validating-an-e-mail-address/
-    NSString *stricterFilterString = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSString *stricterFilterString = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}";
     NSString *laxString = @".+@.+\\.[A-Za-z]{2}[A-Za-z]*";
     NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
